31a895b4995f97d81dcb7ef006c4bf16363e6a0a
Sam Ramirez <ramirezsam99@gmail.com>
2020-01-22 14:49:43 -0800

    Added simply blog setup

---
+++ app/routes.py
@@ -0,0 +1,13 @@
+#import flask
+from app import app
+
+#home page
+
+@app.route("/")
+
+def home():
+    return '<h1>My Blog</h1>'
+    
+@app.route('/about')
+def about():
+    return '<h1>About Me</h1>'
